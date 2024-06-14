c2ea7a9e08f856fffdd4f3abf0ba78f3e7263d70
aadv1k <aadv1k@outlook.com>
2024-06-14 11:04:55 +0530

    Implement the user login system

--- server/users/views.py
+++ server/users/views.py
@@ -1,21 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from datetime import datetime, timedelta, timezone
+
+from django.conf import settings
+from django.db.models import F
+from django.shortcuts import get_object_or_404
+
+import jwt
+import random
+
+from typing import NamedTuple
+from win10toast import ToastNotifier
 
 from .serializers import UserLoginSerializer, UserVerificationSerializer
 from .models import User
 from api.exceptions import HTTPSerializerBadRequest, HTTPBadRequest
 
-import random
+toaster = ToastNotifier()
 
-from typing import NamedTuple
-from datetime import datetime, timedelta
-
-from win10toast import ToastNotifier
-
-def send_windows_notification(title, message):
-    toaster = ToastNotifier()
-    toaster.show_toast(title, message, duration=5)
+TOKEN_EXPIRY_DAYS = 7
 
 class OTPData(NamedTuple):
     otp: str
@@ -45,11 +49,12 @@ def user_login(request):
     if not serializer.is_valid():
         raise HTTPSerializerBadRequest(details=serializer.errors)
 
-    print(serializer.validated_data)
-    otp = generate_otp()
-    mem_store.store(serializer.validated_data.get("phone_number"), otp)
+    phone_number = serializer.validated_data.get("phone_number")
 
-    send_windows_notification("Verify your OTP", f"Here is your OTP, it will expire in 5 minutes {otp}")
+    otp = generate_otp()
+    mem_store.store(phone_number, otp)
+
+    send_windows_notification("Verify your OTP", f"Here is your OTP: {otp}. It will expire in 5 minutes.")
 
     return Response({"message": f"Sent an OTP to the provided mobile number."}, status=status.HTTP_200_OK)
 
@@ -59,22 +64,42 @@ def user_verify(request):
     if not serializer.is_valid():
         raise HTTPSerializerBadRequest(details=serializer.errors)
 
-    stored_data = mem_store.retrieve(serializer.validated_data.get("phone_number"))
+    phone_number = serializer.validated_data.get("phone_number")
+    received_otp = serializer.validated_data.get("otp")
+
+    stored_data = mem_store.retrieve(phone_number)
     if not stored_data:
         raise HTTPBadRequest(message="OTP not found. Please request a new OTP.")
 
     stored_otp, created_at = stored_data.otp, stored_data.created_at
-    
-    received_otp = serializer.validated_data.get("otp")
 
     expiry_time = created_at + timedelta(minutes=5)
     current_time = datetime.now()
     if current_time > expiry_time:
-        mem_store.delete(serializer.validated_data.get("phone_number"))
+        mem_store.delete(phone_number)
         raise HTTPBadRequest(message="OTP has expired. Please request a new OTP.")
 
     if stored_otp != received_otp:
         raise HTTPBadRequest(message="Sorry, the OTP you sent didn't match. Please try again later.")
-    
-    mem_store.delete(serializer.validated_data.get("phone_number"))
-    return Response({"message": "OTP verification successful."})
+
+    mem_store.delete(phone_number)
+
+    user = get_object_or_404(User, phone_number=phone_number)
+    access_token = get_access_token(user.user_id, user.first_name)
+
+    return Response({"access_token": access_token}, status=status.HTTP_200_OK)
+
+def get_access_token(user_id, user_first_name):
+    now = datetime.now(timezone.utc)
+    payload = {
+        'user_id': user_id,
+        'user_first_name': user_first_name,
+        'exp': now + timedelta(days=TOKEN_EXPIRY_DAYS)
+    }
+
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+    return token.decode('utf-8')
+
+def send_windows_notification(title, message):
+    toaster.show_toast(title, message, duration=5)
